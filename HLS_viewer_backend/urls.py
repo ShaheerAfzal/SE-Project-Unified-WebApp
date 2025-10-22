5203043997436f6e8c61d6cf1ea6be893b210573
Shaheer Afzal <shaheerafzal341@gmail.com>
2025-10-22 13:20:37 +0500

    hls tool backend done, integrated with frontend

--- HLS_viewer_backend/urls.py
+++ HLS_viewer_backend/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import StreamViewSet
+from .views import *
 
 router = DefaultRouter()
 router.register(r'streams', StreamViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('viewer/', HlsIndexView.as_view(), name='hls_index'),
+    path('viewer/gate/<int:stream_id>/', GateCameraView.as_view(), name='gate_camera'),
 ]
